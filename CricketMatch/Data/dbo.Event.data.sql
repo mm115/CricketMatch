57fa2bb3e8cf7aff8aa01ea4d63523486cde53b9
Manik Manik <manik7696591290@gmail.com>
2021-01-25 12:51:39 +1300

    Add project files.

---
+++ CricketMatch/Data/dbo.Event.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Event] ON
+INSERT INTO [dbo].[Event] ([EventID], [Eventname], [StartDate], [EndDate], [EventDuration]) VALUES (1, N'IPL', N'2021-01-26 00:00:00', N'2021-02-26 00:00:00', N'30 days')
+INSERT INTO [dbo].[Event] ([EventID], [Eventname], [StartDate], [EndDate], [EventDuration]) VALUES (2, N'ICC', N'2021-03-01 00:00:00', N'2021-04-01 00:00:00', N'30 days')
+INSERT INTO [dbo].[Event] ([EventID], [Eventname], [StartDate], [EndDate], [EventDuration]) VALUES (3, N'T20', N'2021-01-29 00:00:00', N'2021-02-28 00:00:00', N'29 days')
+SET IDENTITY_INSERT [dbo].[Event] OFF
---
+++ CricketMatch/Migrations/20210124221944_initialcreate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CricketMatch.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    EventID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Eventname = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    EventDuration = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.EventID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fixture",
+                columns: table => new
+                {
+                    Fixture_Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    vs_Team = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Ground_Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fixture", x => x.Fixture_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    PlayerID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerFname = table.Column<string>(nullable: true),
+                    PlayerLname = table.Column<string>(nullable: true),
+                    DoB = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.PlayerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ranking",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerID = table.Column<int>(nullable: false),
+                    EventID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ranking", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Ranking_Event_EventID",
+                        column: x => x.EventID,
+                        principalTable: "Event",
+                        principalColumn: "EventID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ranking_Player_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "Player",
+                        principalColumn: "PlayerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ranking_EventID",
+                table: "Ranking",
+                column: "EventID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ranking_PlayerID",
+                table: "Ranking",
+                column: "PlayerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fixture");
+
+            migrationBuilder.DropTable(
+                name: "Ranking");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
